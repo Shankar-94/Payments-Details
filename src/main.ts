49a32a4ee10debd7e6a0ea7f90b749284789c811
Shankar-94 <121161887+Shankar-94@users.noreply.github.com>
2024-12-12 22:57:20 +0530

    Payment Details Crud Application

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from "@angular/platform-browser/animations"
+import {provideToastr} from "ngx-toastr"
 
-bootstrapApplication(AppComponent, appConfig)
+// Extend appConfig to include HttpClient
+const extendedAppConfig = {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []), // Include existing providers from appConfig
+    provideHttpClient(),           // Add HttpClient here
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: "toast-top-right",
+      preventDuplicates: true,
+    })
+  ],
+};
+
+bootstrapApplication(AppComponent, extendedAppConfig)
   .catch((err) => console.error(err));
